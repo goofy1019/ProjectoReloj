038d8536cde5d9a2d2cdfb4a5fb66680d97d38b0
Adolfo <adolfoamador@gmail.com>
2024-11-04 19:11:53 -0600

    Version 3

--- Reloj/Reloj.ino
+++ Reloj/Reloj.ino
@@ -1,30 +1,12 @@
-/*********************************************
-* Universidad Fidelitas                      *
-* Curso: Programacion y Diseño de Algoritmos *
-* Autor: Adolfo Amador Afonso                *
-*        Edgar Picado                        *
-*        Kevin Esquivel Cartin               *
-* Fecha: 08/11/2024                          *
-* Nombre: Reloj.ino                          *
-**********************************************/
-/////////////////////////////
-// Bibliotecas del sistema //
-////////////////////////////
-
-#include <RTClib.h>   // Biblioteca que maneja los RTC (Real Time Clocks)
 #include <Wire.h>
-
-RTC_DS3231 rtc;
-
-// Define constants for pins
+// Define button pins
 const int setTimeButton = A0;
 const int setAlarmButton = A1;
 const int hourButton = A2;
 const int minuteButton = A3;
 const int secondButton = A4;
-const int buzzerPin = 6;
 
-// Define BCD input pins for the 74LS48 (assuming D2-D5 for one digit)
+// Define BCD input pins for the 74LS48
 const int pinA = 2;  // BCD A
 const int pinB = 3;  // BCD B
 const int pinC = 4;  // BCD C
@@ -38,14 +20,23 @@ const int digit4 = 9;  // Enable pin for the fourth digit (units of minutes)
 const int digit5 = 10; // Enable pin for the fifth digit (tens of seconds)
 const int digit6 = 11; // Enable pin for the sixth digit (units of seconds)
 
-int hours = 0, minutes = 0, seconds = 0;
-int alarmHour = 0, alarmMinute = 0, alarmSecond = 0;
-bool alarmSet = false;
-bool alarmActive = false;
+unsigned long previousMillis = 0;  // Tracks the last time we updated the clock
+int hour = 0, minute = 0, second = 0;  // Time variables
+int alarmHour = 0, alarmMinute = 0, alarmSecond = 0; // Alarm time variables
+
+enum Mode { NORMAL, SET_TIME, SET_ALARM };
+Mode currentMode = NORMAL;  // Initial mode
 
 void setup() {
   // Initialize serial for debugging
   Serial.begin(9600);
+
+  // Initialize button pins without pull-up (assuming external pull-down resistors are used)
+  pinMode(setTimeButton, INPUT);
+  pinMode(setAlarmButton, INPUT);
+  pinMode(hourButton, INPUT);
+  pinMode(minuteButton, INPUT);
+  pinMode(secondButton, INPUT);
 
   // Initialize BCD pins
   pinMode(pinA, OUTPUT);
@@ -61,90 +52,106 @@ void setup() {
   pinMode(digit5, OUTPUT);
   pinMode(digit6, OUTPUT);
 
-  pinMode(setTimeButton, INPUT_PULLUP);
-  pinMode(setAlarmButton, INPUT_PULLUP);
-  pinMode(hourButton, INPUT_PULLUP);
-  pinMode(minuteButton, INPUT_PULLUP);
-  pinMode(secondButton, INPUT_PULLUP);
-  pinMode(buzzerPin, OUTPUT);
-
-  // Initialize RTC
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, setting time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
+  // Initialize all digits to HIGH (inactive)
+  digitalWrite(digit1, HIGH);
+  digitalWrite(digit2, HIGH);
+  digitalWrite(digit3, HIGH);
+  digitalWrite(digit4, HIGH);
+  digitalWrite(digit5, HIGH);
+  digitalWrite(digit6, HIGH);
 }
 
 void loop() {
-  DateTime now = rtc.now();
-  int hour = now.hour();
-  int minute = now.minute();
-  int second = now.second();
-  // Check for button presses to set time or alarm
   handleButtons();
 
-  // Update the display
-  displayTime(hour, minute, second);
-
-  // Check if the alarm should go off
-  if (alarmSet && hours == alarmHour && minutes == alarmMinute && seconds == 0) {
-    activateAlarm();
+  // Update the clock every second if in NORMAL mode
+  if (currentMode == NORMAL && millis() - previousMillis >= 1000) {
+    previousMillis = millis();
+    updateClock();
   }
 
-  delay(1000);
+  // Continuously refresh display for smooth multiplexing
+  int displayHour = (currentMode == SET_ALARM) ? alarmHour : hour;
+  int displayMinute = (currentMode == SET_ALARM) ? alarmMinute : minute;
+  int displaySecond = (currentMode == SET_ALARM) ? alarmSecond : second;
+
+  // Refresh each digit
+  displayMultiplexed(displayHour, displayMinute, displaySecond);
 }
 
 void handleButtons() {
-  if (digitalRead(setTimeButton) == LOW) {
+  // Switch to Set Time Mode
+  if (digitalRead(setTimeButton) == HIGH) {  // Changed to HIGH
     delay(200); // Debounce
-    setTime();
+    currentMode = (currentMode == SET_TIME) ? NORMAL : SET_TIME;
+    Serial.println("Switched to Set Time Mode");
   }
-  if (digitalRead(setAlarmButton) == LOW) {
+
+  // Switch to Set Alarm Mode
+  if (digitalRead(setAlarmButton) == HIGH) {  // Changed to HIGH
     delay(200); // Debounce
-    setAlarm();
+    currentMode = (currentMode == SET_ALARM) ? NORMAL : SET_ALARM;
+    Serial.println("Switched to Set Alarm Mode");
+  }
+
+  // Adjust hours in Set Mode
+  if (digitalRead(hourButton) == HIGH) {  // Changed to HIGH
+    delay(200); // Debounce
+    if (currentMode == SET_TIME) {
+      hour = (hour + 1) % 24;
+    } else if (currentMode == SET_ALARM) {
+      alarmHour = (alarmHour + 1) % 24;
+    }
+    Serial.println("Hour incremented");
+  }
+
+  // Adjust minutes in Set Mode
+  if (digitalRead(minuteButton) == HIGH) {  // Changed to HIGH
+    delay(200); // Debounce
+    if (currentMode == SET_TIME) {
+      minute = (minute + 1) % 60;
+    } else if (currentMode == SET_ALARM) {
+      alarmMinute = (alarmMinute + 1) % 60;
+    }
+    Serial.println("Minute incremented");
+  }
+
+  // Adjust seconds in Set Mode
+  if (digitalRead(secondButton) == HIGH) {  // Changed to HIGH
+    delay(200); // Debounce
+    if (currentMode == SET_TIME) {
+      second = (second + 1) % 60;
+    } else if (currentMode == SET_ALARM) {
+      alarmSecond = (alarmSecond + 1) % 60;
+    }
+    Serial.println("Second incremented");
   }
 }
 
-void setTime() {
-  if (digitalRead(hourButton) == LOW) {
-    hours = (hours + 1) % 24;
-    delay(200);
+void updateClock() {
+  second++;
+  if (second >= 60) {
+    second = 0;
+    minute++;
+    if (minute >= 60) {
+      minute = 0;
+      hour++;
+      if (hour >= 24) {
+        hour = 0;  // Reset to 00:00:00 at midnight
+      }
+    }
   }
-  if (digitalRead(minuteButton) == LOW) {
-    minutes = (minutes + 1) % 60;
-    delay(200);
-  }
-  if (digitalRead(secondButton) == LOW) {
-    seconds = (seconds + 1) % 60;
-    delay(200);
-  }
-  displayTime(hours, minutes, seconds);
+
+  // Debugging: Print the current time to Serial Monitor
+  Serial.print("Time: ");
+  Serial.print(hour);
+  Serial.print(":");
+  Serial.print(minute);
+  Serial.print(":");
+  Serial.println(second);
 }
 
-void setAlarm() {
-  if (digitalRead(hourButton) == LOW) {
-    alarmHour = (alarmHour + 1) % 24;
-    delay(200);
-  }
-  if (digitalRead(minuteButton) == LOW) {
-    alarmMinute = (alarmMinute + 1) % 60;
-    delay(200);
-  }
-  displayTime(alarmHour, alarmMinute, 0);  // Display the hour and minute only
-}
-
-void activateAlarm() {
-  digitalWrite(buzzerPin, HIGH);
-  delay(1000);  // Buzzer sound duration
-  digitalWrite(buzzerPin, LOW);
-}
-
-// Function to display the time on 7-segment display
-void displayTime(int hour, int minute, int second) {
+void displayMultiplexed(int hour, int minute, int second) {
   // Separate hours, minutes, and seconds into individual digits
   int digits[] = {
     hour / 10, hour % 10,   // Tens and units of hours
@@ -152,16 +159,15 @@ void displayTime(int hour, int minute, int second) {
     second / 10, second % 10   // Tens and units of seconds
   };
 
-  // Display each digit by enabling one at a time and sending BCD values
-  displayDigit(digits[0], digit1);  // Tens of hours
-  displayDigit(digits[1], digit2);  // Units of hours
-  displayDigit(digits[2], digit3);  // Tens of minutes
-  displayDigit(digits[3], digit4);  // Units of minutes
-  displayDigit(digits[4], digit5);  // Tens of seconds
-  displayDigit(digits[5], digit6);  // Units of seconds
+  // Display each digit briefly for multiplexing
+  displayDigit(digits[0], digit1);
+  displayDigit(digits[1], digit2);
+  displayDigit(digits[2], digit3);
+  displayDigit(digits[3], digit4);
+  displayDigit(digits[4], digit5);
+  displayDigit(digits[5], digit6);
 }
 
-// Function to display a single digit on the 74LS48
 void displayDigit(int number, int digitPin) {
   // Convert number to BCD and output to the 74LS48
   digitalWrite(pinA, (number & 0x1) ? HIGH : LOW);
@@ -169,8 +175,8 @@ void displayDigit(int number, int digitPin) {
   digitalWrite(pinC, (number & 0x4) ? HIGH : LOW);
   digitalWrite(pinD, (number & 0x8) ? HIGH : LOW);
 
-  // Enable the correct digit
-  digitalWrite(digitPin, HIGH);
-  delay(2);  // Brief delay to stabilize the display
-  digitalWrite(digitPin, LOW);  // Turn off digit to avoid ghosting
+  // Enable the correct digit (active LOW)
+  digitalWrite(digitPin, LOW);
+  delayMicroseconds(500); // Brief delay to stabilize the display
+  digitalWrite(digitPin, HIGH); // Turn off digit to avoid ghosting
 }
